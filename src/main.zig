eb92a11734f93b3bb6a79be3f40fb0c83fd69c55
seg4lt <12027886+seg4lt@users.noreply.github.com>
2025-11-07 11:47:13 -0600

    Add support for diff variant of types

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,42 @@ const std = @import("std");
 const claptain = @import("claptain");
 
 const CliArgs = struct {
-    base_path: []const u8 = "asdf",
-    run: bool = true,
-    tag: enum { foo, bar } = .foo,
-    src_path: []const u8,
+    const_u8_required: []const u8,
+    const_u8_default: []const u8 = "const_u8_default",
+    opt_const_u8: ?[]const u8,
+    opt_const_u8_default: ?[]const u8 = "opt_const_u8_default",
+    
+    bool_required: bool,
+    bool_default: bool = false,
+    opt_bool: ?bool,
+    opt_bool_default: ?bool = true,
+    
+    
+    enum_required: enum { foo, bar },
+    enum_default: enum { foo, bar } = .bar,
+    opt_enum: ?enum { foo, bar },
+    opt_enum_default: ?enum { foo, bar } = .foo,
+    
+    
 };
 
 pub fn main() !void {
     const args = try claptain.parse(CliArgs, .{});
-    std.log.debug("--`{s}`--", .{args.base_path});
+    structPrinter(args);
+}
+
+fn structPrinter(value: anytype) void {
+    const T = @TypeOf(value);
+    inline for (std.meta.fields(T)) |field| {
+        switch (@typeInfo(field.type)) {
+            .pointer => |ptr| {
+                if (ptr.size == .slice and ptr.child == u8) {
+                    std.log.debug("{s:>20} = `{s}`", .{ field.name, @field(value, field.name) });
+                    continue;
+                }
+                structPrinter(@field(value, field.name).*);
+            },
+            else => std.log.debug("{s:>20} = `{any}`", .{ field.name, @field(value, field.name) }),
+        }
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -9,6 +9,7 @@ pub fn parse(comptime T: type, option: ParseOptions) ParseError!T {
         .writer = &stderr_state.interface,
         .option = option,
     };
+    defer self.writer.flush() catch {};
 
     const fields = std.meta.fields(T);
     const num_fields = fields.len;
@@ -16,6 +17,7 @@ pub fn parse(comptime T: type, option: ParseOptions) ParseError!T {
 
     var result: T = undefined;
 
+    // Mark all optional and fields with default value as seen, also set default value if exist
     inline for (fields, 0..) |field, i| {
         const is_optional = @typeInfo(field.type) == .optional;
         const has_default = field.default_value_ptr != null;
@@ -27,11 +29,28 @@ pub fn parse(comptime T: type, option: ParseOptions) ParseError!T {
         }
     }
 
+    var args = std.process.args();
+    _ = args.next(); // skip exe name
+
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, option.usage_flag)) {
+            self.printUsage(T) catch {};
+            std.process.exit(option.exit_status_code);
+        }
+        try self.parseFieldValue(T, &result, &fields_seen, arg);
+    }
+
+    // Verify all required arguments are provided
+    var missing_arg = false;
     inline for (fields, 0..) |field, i| {
         if (!fields_seen[i]) {
-            try self.print("Required argument '{s}' missing\n", .{field.name});
-            self.printUsage(T) catch std.process.exit(option.exit_status_code);
+            missing_arg = true;
+            try self.print("required argument '{s}' missing\n", .{field.name});
         }
+    }
+    if (missing_arg) {
+        self.printUsage(T) catch {};
+        std.process.exit(option.exit_status_code);
     }
 
     return result;
@@ -53,19 +72,97 @@ const ClaptainParser = struct {
     writer: *std.Io.Writer,
     option: ParseOptions,
 
-    fn print(s: *const @This(), comptime fmt: []const u8, args: anytype) ParseError!void {
-        s.writer.print(fmt, args) catch return ParseError.PrintFailed;
-    }
+    fn parseFieldValue(self: *const @This(), comptime T: type, result: *T, fields_seen: []bool, arg: []const u8) ParseError!void {
+        const index_of_equal = std.mem.indexOf(u8, arg, "=");
+        const field_identifier = if (index_of_equal) |idx| arg[0..idx] else arg;
 
-    fn flush(s: *const @This()) ParseError!void {
-        s.writer.flush() catch return ParseError.PrintFailed;
+        if (!std.mem.startsWith(u8, field_identifier, "--") and !self.option.allow_invalid) {
+            try self.print("options should start with `--` found `{s}`", .{field_identifier});
+            return ParseError.InvalidArgument;
+        }
+
+        const field_name = field_identifier[2..];
+        var field_found = false;
+
+        inline for (std.meta.fields(T), 0..) |field, i| {
+            if (std.mem.eql(u8, field.name, field_name)) {
+                field_found = true;
+                defer if (field_found) {
+                    fields_seen[i] = true;
+                };
+
+                switch (@typeInfo(field.type)) {
+                    .pointer => |ptr| {
+                        const is_u8_slice = ptr.size == .slice and ptr.child == u8;
+                        if (!is_u8_slice) @compileError("only []u8 pointer type is supported for string fields.");
+
+                        if (index_of_equal == null) {
+                            try self.print("Missing value for argument '{s}'\n", .{field_name});
+                            self.printUsage(T) catch {};
+                            return ParseError.InvalidArgument;
+                        }
+                        const initial_value = arg[index_of_equal.? + 1 ..];
+                        if (std.mem.startsWith(u8, initial_value, "\"") and std.mem.endsWith(u8, initial_value, "\"")) {
+                            @field(result, field.name) = initial_value[1 .. initial_value.len - 1];
+                        } else {
+                            @field(result, field.name) = initial_value;
+                        }
+                    },
+                    .@"enum" => |enum_info| {
+                        if (index_of_equal == null) {
+                            try self.print("Missing value for argument '{s}'\n", .{field_name});
+                            self.printUsage(T) catch {};
+                            return ParseError.InvalidArgument;
+                        }
+                        const value_str = arg[index_of_equal.? + 1 ..];
+                        var matched = false;
+                        inline for (enum_info.fields) |enum_field| {
+                            if (std.mem.eql(u8, enum_field.name, value_str)) {
+                                @field(result, field.name) = @enumFromInt(enum_field.value);
+                                matched = true;
+                                break;
+                            }
+                        }
+                        if (!matched) {
+                            try self.print("Invalid value '{s}' for argument '{s}'\n", .{ value_str, field_name });
+                            self.printUsage(T) catch {};
+                            return ParseError.InvalidArgument;
+                        }
+                    },
+                    .bool => {
+                        if (index_of_equal == null) {
+                            @field(result, field.name) = true;
+                            return;
+                        }
+                        const value_str = arg[index_of_equal.? + 1 ..];
+                        if (std.mem.eql(u8, value_str, "true")) {
+                            @field(result, field.name) = true;
+                        } else if (std.mem.eql(u8, value_str, "false")) {
+                            @field(result, field.name) = false;
+                        } else {
+                            try self.print("invalid boolean value '{s}' for argument '{s}'\n", .{ value_str, field_name });
+                            self.printUsage(T) catch {};
+                            return ParseError.InvalidArgument;
+                        }
+                    },
+                    else => |tag| std.debug.panic("not implemented: {s}\n", .{@tagName(tag)}),
+                }
+                break;
+            }
+        }
+
+        if (!field_found) {
+            if (self.option.allow_invalid) return;
+            try self.print("unknown argument '{s}'\n", .{field_name});
+            return ParseError.InvalidArgument;
+        }
     }
 
     fn printUsage(self: *const @This(), comptime T: type) !void {
         var args = std.process.args();
         const exe_name = args.next().?; // exe name should always exist
 
-        try self.print("Usage: {s} [OPTIONS]\n\nOptions:\n", .{std.fs.path.basename(exe_name)});
+        try self.print("usage: {s} [OPTIONS]\n\nOPTIONS:\n", .{std.fs.path.basename(exe_name)});
         try self.flush();
 
         try self.printUsageForStructType(T);
@@ -87,21 +184,17 @@ const ClaptainParser = struct {
             switch (@typeInfo(actual_type)) {
                 .pointer => |ptr| {
                     const is_u8_slice = ptr.size == .slice and ptr.child == u8;
-                    if (!is_u8_slice) continue;
+                    if (!is_u8_slice) @compileError("only []u8 pointer type is supported for string fields.");
 
                     try self.print("\t", .{});
                     try self.print("--{s}=<string>", .{field.name});
-
                     try self.printAdditionalUsageInfo(has_default, is_required, field);
-
                     try self.print("\n", .{});
                 },
                 .bool => {
                     try self.print("\t", .{});
-                    try self.print("--{s} or --{s}=<bool>", .{ field.name, field.name });
-
+                    try self.print("--{s} or --{s}=true|false", .{ field.name, field.name });
                     try self.printAdditionalUsageInfo(has_default, is_required, field);
-
                     try self.print("\n", .{});
                 },
                 .@"enum" => |enum_info| {
@@ -123,14 +216,33 @@ const ClaptainParser = struct {
     }
 
     fn printAdditionalUsageInfo(self: *const @This(), has_default: bool, is_required: bool, field: std.builtin.Type.StructField) ParseError!void {
+        try self.print("\t(required={any})", .{is_required});
         if (has_default) {
             const value = @as(*const field.type, @ptrCast(@alignCast(field.default_value_ptr.?))).*;
             switch (@typeInfo(field.type)) {
                 .@"enum" => try self.print("\t(default: \"{s}\")", .{@tagName(value)}),
-                else => try self.print("\t(default: \"{any}\")", .{value}),
+                .bool => try self.print("\t(default: \"{any}\")", .{value}),
+                .optional => {
+                    // TODO(seg4lt) - can we just recursively call printAdditionalUsageInfo here?
+                    // types are screwed, need to experiment further
+                    if (value) |unwrapped| {
+                        const child_type = @typeInfo(field.type).optional.child;
+                        switch (@typeInfo(child_type)) {
+                            .@"enum" => try self.print("\t(default: \"{s}\")", .{@tagName(unwrapped)}),
+                            .bool => try self.print("\t(default: \"{any}\")", .{unwrapped}),
+                            else => try self.print("\t(default: \"{s}\")", .{unwrapped}),
+                        }
+                    }
+                },
+                else => try self.print("\t(default: \"{s}\")", .{value}),
             }
-        } else if (is_required) {
-            try self.print("\t(required)", .{});
         }
+    }
+    fn print(s: *const @This(), comptime fmt: []const u8, args: anytype) ParseError!void {
+        s.writer.print(fmt, args) catch return ParseError.PrintFailed;
+    }
+
+    fn flush(s: *const @This()) ParseError!void {
+        s.writer.flush() catch return ParseError.PrintFailed;
     }
 };
